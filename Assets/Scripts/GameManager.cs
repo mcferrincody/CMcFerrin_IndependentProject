0cfedfb3137f0049ecd16fec68933db6d04ca432
mcferrincody <61605145+mcferrincody@users.noreply.github.com>
2021-05-17 22:39:02 -0700

    Script comments explaining player movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject spider_pref;
+    public bool gameActive = true;
+    GameEnding m_GameEnding;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_GameEnding = GameObject.Find("GameEnding").GetComponent<GameEnding>();
+        InvokeRepeating("SpawnSpider", 2.0f, 5.0f);
+    }
+
+    // Update is called once per frame
+    void SpawnSpider()
+    {
+        if (gameActive)
+        {
+            Instantiate(spider_pref);
+        }
+    }
+    public void LoseGame()
+    {
+        m_GameEnding.CaughtPlayer();
+        gameActive = false;
+        Debug.Log("Lose Game");
+    }
+}
---
+++ Assets/Scripts/PlayerCaught.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCaught : MonoBehaviour
+{
+    GameManager m_GameManager;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Caught by Spider");
+            m_GameManager.LoseGame();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_GameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
